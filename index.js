d3b337632a66b8815e1a96a23b34090eb97d3a12
Gregor Martynus <gregor@martynus.net>
2015-09-30 10:46:22 -0400

    feat: initial version

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = Log
+
+Log.console = console
+
+var log = require('./lib/log')
+var debug = require('./lib/debug')
+var info = require('./lib/info')
+var warn = require('./lib/warn')
+var error = require('./lib/error')
+
+var parseOptions = require('./lib/utils/parse-options')
+
+function Log (options) {
+  var state = parseOptions(options)
+  state.console = Log.console
+
+  var api = log.bind(null, state)
+
+  api.debug = debug.bind(null, state)
+  api.info = info.bind(null, state)
+  api.warn = warn.bind(null, state)
+  api.error = error.bind(null, state)
+  api.scoped = function (name) {
+    return Log({
+      prefix: state.prefix + ':' + name,
+      level: state.level,
+      styles: state.styles
+    })
+  }
+
+  api.prefix = state.prefix
+  api.level = state.level
+
+  return api
+}
