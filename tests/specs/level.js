f9b12c4a0dc6e9f284d356ba76e2ef7aa61f33f6
ianstalter123 <istalter66@gmail.com>
2015-12-18 00:46:08 -0800

    test: log level will not set with invalid level
    amended test to throw error

--- tests/specs/level.js
+++ tests/specs/level.js
@@ -7,7 +7,6 @@ test('log.level', function (t) {
   Log.console = {
     log: simple.stub()
   }
-
   var log = new Log('foo')
   log.level = 'warn'
   log.debug('bar')
@@ -15,9 +14,20 @@ test('log.level', function (t) {
   log.debug('baz')
   var barLog = log.scoped('bar')
   barLog.debug('baz')
-
   t.is(Log.console.log.callCount, 2, 'log.debug only called after level set to "debug"')
   t.end()
+  Log.console = console
+})
 
+test('log.level with invalid name will not change', function (t) {
+  Log.console = {
+    log: simple.stub()
+  }
+  var log = new Log('foo')
+
+  t.throws(function () {
+    log.level = 'test'
+  })
+  t.end()
   Log.console = console
 })
